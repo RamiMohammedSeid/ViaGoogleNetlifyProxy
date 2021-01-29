305bbc7fb9459868a756ee6a8c03d46f520da964
Rami Mohammed <seidramimohammed@gmail.com>
2021-01-30 00:05:52 +0300

    Some changes

--- src/lambda/proxy.js
+++ src/lambda/proxy.js
@@ -1,34 +1,24 @@
-const { GROUP_IDS, ACCESS_TOKEN } = process.env;
-
 import fetch from "node-fetch";
 
 import flatten from "lodash/flatten";
 
-const getEvents = ({ groupId }) => {
-  const endpoint = `https://api.meetup.com/${groupId}/events?photo-host=public&page=20`;
+const getGoogle = ( query ) => {
+  const endpoint = `http://www.google.com/search?q=${query}`;
   return fetch(endpoint, {
-    headers: { Authorization: `Bearer ${ACCESS_TOKEN}` }
-  }).then(response => response.json());
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Accept-Encoding': 'gzip, deflate',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'lang': 'en-us'
+    }
+  }).then(response => { return { statusCode: 200, body: response.text() } });
 };
 
 exports.handler = async (event, context) => {
-  const groupIds = GROUP_IDS.split(",");
 
-  const promises = groupIds.map(groupId =>
-    getEvents({
-      groupId: groupId
-    })
-  );
-
-  return Promise.all(promises)
-    .then(data => ({
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept"
-      },
-      statusCode: 200,
-      body: JSON.stringify(flatten(data))
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+  return getGoogle(event.queryStringParameters.query)
 };
