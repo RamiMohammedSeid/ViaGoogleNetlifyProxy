8f1f2e5d150d4e9081b5a91d7854d3d14eaecf97
Rami Mohammed <seidramimohammed@gmail.com>
2021-02-16 14:03:26 +0300

    changes

--- src/lambda/proxy.js
+++ src/lambda/proxy.js
@@ -3,14 +3,13 @@ import fetch from "node-fetch";
 import flatten from "lodash/flatten";
 
 const getGoogle = ( query ) => {
-  const endpoint = `http://www.google.com/search?q=${query}`;
+  const endpoint = `http://www.google.com/search?q=${query.replace(" ","+")}`;
   return fetch(endpoint, {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4412.3 Safari/537.36',
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
       'Accept-Language': 'en-US,en;q=0.5',
       'Accept-Encoding': 'gzip, deflate',
-      'DNT': '1',
       'Connection': 'keep-alive',
       'Upgrade-Insecure-Requests': '1',
       'lang': 'en-us'
